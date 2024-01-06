3e00fde8a207ef76277ecec4f4a7a6b7e75762b5
Khadija Ibrahim <88938812+KhadijaIbrahim123@users.noreply.github.com>
2024-01-06 20:55:18 +0500

    laravel blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,3 +2,19 @@
 
 <a href="/post">post</a>
 <a href="{{route("blog")}}">blog</a>
+
+
+{{-- ALERT --}}
+{!!"<script>alert('Khadija Ibrahima')</script>"!!}
+
+
+
+@php
+   $names = ['khadija','uzair','sara','zainab'] 
+@endphp
+<ul>
+@foreach ($names as $n)
+<li>{{$n}}</li>
+    
+@endforeach
+</ul> 
